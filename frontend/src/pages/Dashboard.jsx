9a7d768c3836a75950576a61a63572b1804bd263
AnujD2506 <anujkdixit999@gmail.com>
2026-04-26 11:12:42 +0000

    frontend update

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,4 @@
+// pages/Dashboard.js
+export default function Dashboard() {
+  return <h1>Welcome to Hostel Dashboard</h1>;
+}
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("role", res.data.role);
+
+      alert("Login successful");
+    } catch (err) {
+      alert(err.response?.data?.message || "Error");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
